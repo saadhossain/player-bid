35ad076bc56ebf31f34d34d44cb94903c17f86c5
saadhossain <mshsarkar@gmail.com>
2022-08-22 02:07:26 +0600

    Error Handler and alert added to prevent invalid input

--- js/calculation.js
+++ js/calculation.js
@@ -1,6 +1,13 @@
 //Calculate total Player expense
 document.getElementById('calculate-btn').addEventListener('click', function(){
     const perPlayerCost = getInputValue('per-player-field');
+
+    //Error Handler to prevent invalid amount
+    if(isNaN(perPlayerCost)){
+        alert('UFF!! Please Provide a Valid Amount');
+        document.getElementById('per-player-field').value = '';
+        return;
+    }
     //Get the total Selected Player in the List
     const selectedPlayer = document.getElementById('selected-player-count').innerText;
     const selectedPlayerCount = parseFloat(selectedPlayer);
@@ -14,7 +21,13 @@ document.getElementById('calculate-total-btn').addEventListener('click', functio
     const totalPlayerExpense = getPlaceholderAmount('player-expense');
     const managerCost = getInputValue('manager-field');
     const coachCost = getInputValue('coach-field');
-    
+    //Error Handler to prevent invalid amount
+    if(isNaN(managerCost) || isNaN(coachCost)){
+        alert('UFF!! Please Provide a Valid Amount');
+        document.getElementById('manager-field').value = '';
+        document.getElementById('coach-field').value = '';
+        return;
+    }
     //Final total Expense for all Player, Manager and Coach
     const finalTotal = totalPlayerExpense + managerCost + coachCost;
     //Set the final Total Amount to respective placeholder to show output/frontend
