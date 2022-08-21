c0666b9225b6375dbfd9090b3336df940c3e08e0
saadhossain <mshsarkar@gmail.com>
2022-08-22 00:58:15 +0600

    Error handler added for, if selected disabled the select button and if anyone try to add more than 5 player he will see an alert and cant add more than 5 player to the list

--- js/selection.js
+++ js/selection.js
@@ -1,18 +1,27 @@
 const allPlayer = [];
 //Common function to add the player to the selected list
-function selectPlayer(element){
-    const player = element.parentNode.parentNode.children[0].innerText;
-    const playerCount = document.getElementById('selected-player-count').innerText = allPlayer.length+1;
+function selectPlayer(element) {
+    if (allPlayer.length === 5) {
+        alert('Sorry! You have Reached Maximum Selection Limit');
+        return;
+    }
+    else {
+        const player = element.parentNode.parentNode.children[0].innerText;
+        const playerCount = document.getElementById('selected-player-count').innerText = allPlayer.length + 1;
 
-    //Push the data to the array
-    allPlayer.push(player);
+        //Push the data to the array
+        allPlayer.push(player);
 
-    const listContainer = document.getElementById('selected-container');
+        const listContainer = document.getElementById('selected-container');
 
-    // listContainer.innerHTML = '';
-    const playerSelect = document.createElement('ul');
-    playerSelect.innerHTML = `
+        // listContainer.innerHTML = '';
+        const playerSelect = document.createElement('ul');
+        playerSelect.innerHTML = `
         <li class="mx-4 my-1 font-semibold"><span>${playerCount}.</span> ${player}</li>
         `;
-    listContainer.appendChild(playerSelect);
+        listContainer.appendChild(playerSelect);
+        //Disabled Button After Added
+        element.setAttribute('disabled', true);
+        element.style.backgroundColor = '#698bd5';
+    }
 }
